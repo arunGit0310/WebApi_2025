416ef377e34cb0c51b17f3ee17ffce19dfc2b08d
DELL <DELL@DESKTOP-FMQABVS>
2025-05-02 14:11:48 +0530

    Add project files.

---
+++ WebApi_2025/Model/StudentModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi_2025.Model
+{
+    public class StudentModel
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public DateTime DOB {  get; set; }
+        public int GenderId {  get; set; }
+        public string GenderName { get; set; }=string.Empty;
+        public int BranchId {  get; set; }
+        public string BranchName { get; set; } = string.Empty;
+    }
+}
